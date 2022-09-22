e36efb8856ce992c494d38b0e1827293981b3e64
Dave Carr <davecarr1024@gmail.com>
2022-09-22 11:57:22 -0400

    rule tests

--- core/processor.py
+++ core/processor.py
@@ -1,14 +1,23 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Generic, Mapping, MutableSequence, Optional, Sequence, TypeVar
+from typing import Any, Generic, Mapping, MutableSequence, Optional, Sequence, TypeVar
 
 
-@dataclass(frozen=True)
+def _repr(type: str, **fields: Any) -> str:
+    fields_str = ','.join(
+        [f'{name}={repr(val)}' for name, val in fields.items() if val is not None])
+    return f'{type}({fields_str})'
+
+
+@dataclass(frozen=True, repr=False)
 class Error(Exception):
     msg: Optional[str] = field(kw_only=True, default=None)
     rule_name: Optional[str] = field(kw_only=True, default=None)
     children: Sequence['Error'] = field(
         kw_only=True, default_factory=lambda: list[Error]())
+
+    def __repr__(self) -> str:
+        return _repr(self.__class__.__name__, msg=self.msg, rule_name=self.rule_name, children=self.children or None)
 
     def with_rule_name(self, rule_name: str) -> 'Error':
         return Error(msg=self.msg, rule_name=rule_name, children=self.children)
@@ -18,15 +27,27 @@ _ResultValue = TypeVar('_ResultValue')
 _StateValue = TypeVar('_StateValue')
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, repr=False)
 class Result(Generic[_ResultValue, _StateValue]):
     value: Optional[_ResultValue] = field(default=None, kw_only=True)
     rule_name: Optional[str] = field(kw_only=True, default=None)
     children: Sequence['Result[_ResultValue,_StateValue]'] = field(
         kw_only=True, default_factory=lambda: list[Result[_ResultValue, _StateValue]]())
 
+    def __repr__(self) -> str:
+        return _repr(self.__class__.__name__, value=self.value, rule_name=self.rule_name, children=self.children or None)
+
     def with_rule_name(self, rule_name: str) -> 'Result[_ResultValue,_StateValue]':
         return Result[_ResultValue, _StateValue](value=self.value, rule_name=rule_name, children=self.children)
+
+    def as_child_result(self) -> 'Result[_ResultValue,_StateValue]':
+        return Result[_ResultValue, _StateValue](children=[self])
+
+    def simplify(self) -> 'Result[_ResultValue,_StateValue]':
+        if self.value is None and self.rule_name is None and len(self.children) == 1:
+            return self.children[0]
+        else:
+            return Result[_ResultValue, _StateValue](value=self.value, rule_name=self.rule_name, children=[child.simplify() for child in self.children])
 
 
 @dataclass(frozen=True, repr=False)
@@ -35,7 +56,7 @@ class State(Generic[_ResultValue, _StateValue]):
     value: _StateValue
 
     def __repr__(self) -> str:
-        return f'State({self.value})'
+        return _repr(self.__class__.__name__, value=self.value)
 
 
 @dataclass(frozen=True)
@@ -47,13 +68,16 @@ class ResultAndState(Generic[_ResultValue, _StateValue]):
         return ResultAndState[_ResultValue, _StateValue](self.result.with_rule_name(rule_name), self.state)
 
     def as_child_result(self) -> 'ResultAndState[_ResultValue,_StateValue]':
-        return ResultAndState[_ResultValue, _StateValue](Result[_ResultValue, _StateValue](children=[self.result]), self.state)
+        return ResultAndState[_ResultValue, _StateValue](self.result.as_child_result(), self.state)
+
+    def simplify(self) -> 'ResultAndState[_ResultValue,_StateValue]':
+        return ResultAndState[_ResultValue, _StateValue](self.result.simplify(), self.state)
 
 
 class Rule(Generic[_ResultValue, _StateValue], ABC):
     @abstractmethod
     def apply(self, state: State[_ResultValue, _StateValue]
-              ) -> ResultAndState[_ResultValue, _StateValue]: pass
+              ) -> ResultAndState[_ResultValue, _StateValue]: ...
 
 
 @dataclass(frozen=True)
@@ -61,23 +85,20 @@ class Processor(Generic[_ResultValue, _StateValue]):
     root_rule_name: str
     rules: Mapping[str, Rule[_ResultValue, _StateValue]]
 
-    def apply_rule_name(self, rule_name: str, state: State[_ResultValue, _StateValue]) -> ResultAndState[_ResultValue, _StateValue]:
+    def apply_rule_name_to_state(self, rule_name: str, state: State[_ResultValue, _StateValue]) -> ResultAndState[_ResultValue, _StateValue]:
         if rule_name not in self.rules:
             raise Error(msg=f'unknown rule {rule_name}')
         try:
-            return self.rules[rule_name].apply(state).with_rule_name(rule_name)
+            return self.rules[rule_name].apply(state).with_rule_name(rule_name).simplify()
         except Error as error:
             raise Error(rule_name=rule_name, children=[error])
 
-    def apply_root(self, state: State[_ResultValue, _StateValue]) -> ResultAndState[_ResultValue, _StateValue]:
-        return self.apply_rule_name(self.root_rule_name, state)
+    def apply_root_to_state(self, state: State[_ResultValue, _StateValue]) -> ResultAndState[_ResultValue, _StateValue]:
+        return self.apply_rule_name_to_state(self.root_rule_name, state)
 
-    def apply(self, state_value: _StateValue) -> _ResultValue:
-        result_value = self.apply_root(
-            State[_ResultValue, _StateValue](self, state_value)).result.value
-        if not result_value:
-            raise Error(msg=f'null result')
-        return result_value
+    def apply_root_to_state_value(self, state_value: _StateValue) -> Result[_ResultValue, _StateValue]:
+        return self.apply_root_to_state(
+            State[_ResultValue, _StateValue](self, state_value)).result
 
 
 @dataclass(frozen=True)
@@ -86,7 +107,7 @@ class Ref(Rule[_ResultValue, _StateValue]):
 
     def apply(self, state: State[_ResultValue, _StateValue]
               ) -> ResultAndState[_ResultValue, _StateValue]:
-        return state.processor.apply_rule_name(self.value, state).as_child_result()
+        return state.processor.apply_rule_name_to_state(self.value, state).as_child_result()
 
 
 @dataclass(frozen=True)
--- core/processor_test.py
+++ core/processor_test.py
@@ -1,11 +1,18 @@
+from abc import ABC, abstractproperty
+from dataclasses import dataclass
+from typing import Generic, Tuple, TypeVar
 from . import processor
 
 import unittest
 
-_Result = processor.Result[int, int]
-_State = processor.State[int, int]
-_ResultAndState = processor.ResultAndState[int, int]
 _Processor = processor.Processor[int, int]
+_Result = processor.Result[int, int]
+_ResultAndState = processor.ResultAndState[int, int]
+_Rule = processor.Rule[int, int]
+_State = processor.State[int, int]
+_Ref = processor.Ref[int, int]
+_And = processor.And[int, int]
+_Or = processor.Or[int, int]
 
 
 class ErrorTest(unittest.TestCase):
@@ -23,11 +30,165 @@ class ResultTest(unittest.TestCase):
             _Result().with_rule_name('a'),
         )
 
-
-class ResultAndStateTest(unittest.TestCase):
-    def test_with_rule_name(self):
-        p = _Processor('', {})
+    def test_as_child_result(self):
         self.assertEqual(
-            _ResultAndState(_Result(), _State(p, 0)).with_rule_name('a'),
-            _ResultAndState(_Result(rule_name='a'), _State(p, 0))
+            _Result(value=1).as_child_result(),
+            _Result(children=[_Result(value=1)])
         )
+
+    def test_simplify(self):
+        for input, expected in list[Tuple[_Result, _Result]]([
+            (_Result(value=1), _Result(value=1)),
+            (_Result(rule_name='a'), _Result(rule_name='a')),
+            (_Result(), _Result()),
+            (_Result(children=[_Result(value=1)]), _Result(value=1)),
+        ]):
+            with self.subTest(input=input, expected=expected):
+                actual = input.simplify()
+                self.assertEqual(expected, actual)
+
+
+_ResultValue = TypeVar('_ResultValue')
+_StateValue = TypeVar('_StateValue')
+
+
+class ProcessorTestCase(Generic[_ResultValue, _StateValue], ABC, unittest.TestCase):
+    def state(self, state_value: _StateValue) -> processor.State[_ResultValue, _StateValue]:
+        return processor.State[_ResultValue, _StateValue](self.processor, state_value)
+
+    @abstractproperty
+    def processor(self) -> processor.Processor[_ResultValue, _StateValue]: ...
+
+
+_ProcessorTestCase = ProcessorTestCase[int, int]
+
+
+class ResultAndStateTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor('', {})
+
+    def test_with_rule_name(self):
+        self.assertEqual(
+            _ResultAndState(_Result(), self.state(0)).with_rule_name('a'),
+            _ResultAndState(_Result(rule_name='a'), self.state(0))
+        )
+
+    def test_as_child_result(self):
+        self.assertEqual(
+            _ResultAndState(_Result(value=1), self.state(0)).as_child_result(),
+            _ResultAndState(_Result(value=1).as_child_result(), self.state(0))
+        )
+
+
+@dataclass(frozen=True)
+class Multiply(_Rule):
+    value: int
+
+    def apply(self, state: _State) -> _ResultAndState:
+        return _ResultAndState(_Result(value=state.value * self.value), state)
+
+
+@dataclass(frozen=True)
+class Increment(_Rule):
+    def apply(self, state: _State) -> _ResultAndState:
+        return _ResultAndState(_Result(), _State(state.processor, state.value+1))
+
+
+@dataclass(frozen=True)
+class Assert(_Rule):
+    value: int
+
+    def apply(self, state: _State) -> _ResultAndState:
+        if state.value != self.value:
+            raise processor.Error(msg=f'{self.value} != {state.value}')
+        return _ResultAndState(_Result(), state)
+
+
+class MultiplyTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor('a', {'a': Multiply(2)})
+
+    def test_apply(self):
+        self.assertEqual(self.processor.apply_root_to_state_value(3),
+                         _Result(value=6, rule_name='a'))
+
+
+class IncrementTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor('a', {'a': Increment()})
+
+    def test_apply(self):
+        self.assertEqual(self.processor.apply_root_to_state(
+            self.state(1)).state.value, 2)
+
+
+class AssertTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor('a', {'a': Assert(1)})
+
+    def test_apply(self):
+        self.processor.apply_root_to_state_value(1)
+        with self.assertRaises(processor.Error):
+            self.processor.apply_root_to_state_value(2)
+
+
+class RefTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor('a', {'a': _Ref('b'), 'b': Multiply(2)})
+
+    def test_apply(self):
+        self.assertEqual(self.processor.apply_root_to_state_value(3),
+                         _Result(rule_name='a', children=[_Result(rule_name='b', value=6)]))
+
+
+class AndTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor(
+            'a',
+            {
+                'a': _And([
+                    Increment(),
+                    Multiply(2),
+                ]),
+            }
+        )
+
+    def test_apply(self):
+        self.assertEqual(
+            self.processor.apply_root_to_state_value(3),
+            _Result(rule_name='a', children=[_Result(), _Result(value=8)])
+        )
+
+
+class OrTest(_ProcessorTestCase):
+    @property
+    def processor(self) -> _Processor:
+        return _Processor(
+            'a',
+            {
+                'a': _Or([
+                    _And([
+                        Assert(2),
+                        Multiply(2),
+                    ]),
+                    _And([
+                        Assert(3),
+                        Multiply(3),
+                    ]),
+                ]),
+            }
+        )
+
+    def test_apply(self):
+        self.assertEqual(self.processor.apply_root_to_state_value(2),
+                         _Result(rule_name='a', children=[
+                             _Result(children=[_Result(), _Result(value=4)])]))
+        self.assertEqual(self.processor.apply_root_to_state_value(3),
+                         _Result(rule_name='a', children=[
+                             _Result(children=[_Result(), _Result(value=9)])]))
